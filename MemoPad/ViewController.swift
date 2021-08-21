e223d2597d67696ff636ba1e4b8ace5feea944f7
Mattun1212 <kouta.dvlp@icloud.com>
2021-08-21 21:03:02 +0900

    MemoPad completed

--- MemoPad/ViewController.swift
+++ MemoPad/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var contentTectView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func saveMemo(){
+        
+    }
+    
 }
 
